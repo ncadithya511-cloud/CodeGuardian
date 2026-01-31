0206883add8abd1e01032ee6d0464f148dd8744e
adithya nc <ncadithya511@gmail.com>
2026-01-31 03:28:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9889403586-e7ca7",
+  "appId": "1:479948205722:web:2063e8b13172fcdff97923",
+  "apiKey": "AIzaSyAW2VmbKH2PEwfKknzH4QDKDS10HJnu6-A",
+  "authDomain": "studio-9889403586-e7ca7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "479948205722"
+};
